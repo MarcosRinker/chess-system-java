f525344282e92ed12b49f5a8c7b848fd484e331c
Marcos Mello <marcosvinicios_mello@yahoo.com.br>
2022-08-29 20:17:27 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,46 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+
+
+	public int getRow() {
+		return row;
+	}
+
+
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+
+
+	public int getColum() {
+		return column;
+	}
+
+
+
+	public void setColum(int colum) {
+		this.column = colum;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+	
+
+}
